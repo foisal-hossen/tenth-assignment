acdb1e5bda42ca04f7b580fb82bcef82e82f63db
foisal-hossen <farjana01811389904@gmail.com>
2022-10-30 07:05:57 +0600

    hader ,home design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { DarkModeContext } from '../Context/DarkContext';
 
 const Home = () => {
+
+  const [setDark, dark] = useContext(DarkModeContext);
+  
+ 
+  
     return (
         <div>
-     <div className="hero min-h-screen" style={{ backgroundImage: `url("https://www.lighthouselabs.ca/uploads/post/open_graph_image/459/Coding-vs-programming.jpg")` }}>
+     {/* <div className="hero min-h-screen" style={{ backgroundImage: `url("https://www.lighthouselabs.ca/uploads/post/open_graph_image/459/Coding-vs-programming.jpg")` }}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
@@ -13,7 +21,22 @@ const Home = () => {
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
-     </div>
+     </div> */}
+        <nav className= {`  ${dark?" bg-gray-800 text-white t":"bg-gray-200 text-black" }`}>
+        <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+			        <h1 className="mt-20 text-3xl lg:text-5xl font-bold leading-none sm:text-6xl">A Large Platform to get skilled
+			        </h1>
+                    <h3 className="text-lg font-bold mt-2 text-teal-600">Our course is rated excellent by over 5,400 people</h3>
+			        <p className="mt-6 mb-8 text-lg sm:mb-12">Today, more than 10,000 people have already joined in our different courses: programming, Machine Learning and Artificial Intelligence. Explore our outstanding courses and get started to be a hero.
+			        </p>
+			    <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                    <Link to="/courses" className="mb-20 px-8 py-3 text-lg font-semibold rounded bg-indigo-400 hover:bg-indigo-500 text-gray-900">Explore Courses</Link>
+            </div>
+            
+            </div>
+        </nav>
+
+        
  </div>
     );
 };
